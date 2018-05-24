45b9de8c6e78ff24afc422b3fc2191303f6b42b2
Renzo Kottmann <renzo.kottmann@finanzen.bremen.de>
2018-05-24 09:29:13 +0200

    Add first version of better PI parser

---
+++ src/main/java/de/kosit/xmlmutate/mutator/PiDataParser.java
@@ -0,0 +1,29 @@
+package de.kosit.xmlmutate.mutator;
+
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+/**
+ * PiDataParser
+ * @author Renzo Kottmann
+ */
+public class PiDataParser {
+
+    private final static Logger log = LogManager.getLogger(PiDataParser.class);
+
+
+    public Map<String, String> parsePiData(String data) {
+        Map<String, String> dataEntries = new HashMap<String, String>();
+
+for (int i = 0; i < data.length(); i++) {
+
+}
+
+return dataEntries;
+    }
+
+}
