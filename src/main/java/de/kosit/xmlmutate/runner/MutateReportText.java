46dda67098b45920e189cdcb4f30066d8a0b67f9
Renzo Kottmann <renzo.kottmann@finanzen.bremen.de>
2018-05-29 19:21:42 +0200

    A little bit more nice report text

--- src/main/java/de/kosit/xmlmutate/runner/MutateReportText.java
+++ src/main/java/de/kosit/xmlmutate/runner/MutateReportText.java
@@ -22,7 +22,7 @@ public class MutateReportText implements MutateReport {
     private int numDocs = 0;
     private int numMutations = 0;
 
-    private Map<String, List<TestItem>> docReports = new HashMap<String, List<TestItem>>();
+    private Map<String, List<TestItem>> docSchematronReports = new HashMap<String, List<TestItem>>();
 
     private Map<String, String> schemaTestStatements = new HashMap<String, String>();
 
@@ -35,19 +35,23 @@ public class MutateReportText implements MutateReport {
     public void write(String directory, String fileName) throws IOException {
         Path path = Paths.get(directory, fileName);
         try (BufferedWriter writer = Files.newBufferedWriter(path, StandardCharsets.UTF_8)) {
-            writer.write(String.format("Generated %s mutations from %s original documents.", numMutations, numDocs));
+            writer.write(String.format("Generated %s mutations from %s original document(s) in directory %s.",
+                    numMutations, numDocs, config.getOutputDir().toString()));
+            writer.newLine();
+            writer.newLine();
 
-            for (String docName : docReports.keySet()) {
+            for (String docName : docSchematronReports.keySet()) {
                 writer.newLine();
+
                 writer.write("Mutation " + docName);
                 writer.newLine();
-                writer.newLine();
-                writer.write("Schema Test: ");
+                writer.write("XML Schema Test: ");
                 writer.write(schemaTestStatements.get(docName));
                 writer.newLine();
-                writer.write("Schema Tests: ");
                 writer.newLine();
-                for (TestItem item : docReports.get(docName)) {
+                writer.write("Schematron Tests: ");
+                writer.newLine();
+                for (TestItem item : docSchematronReports.get(docName)) {
                     writer.write(item.toString());
                     writer.newLine();
                 }
@@ -68,7 +72,7 @@ public class MutateReportText implements MutateReport {
 
     @Override
     public void addDocumentReport(String docName, List<TestItem> testItems) {
-        docReports.put(docName, testItems);
+        docSchematronReports.put(docName, testItems);
     }
 
     @Override
