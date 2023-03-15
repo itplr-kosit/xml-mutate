7243e2cd83125419c87958999210352b8c7e4448
Tomas Baltrukonis <tomas.baltrukonis@nortal.com>
2023-03-15 14:10:01 +0000

    Resolve "mutator does not handle invalid xmute instructions"

--- src/test/java/de/kosit/xmlmutate/runner/MutationRunnerTest.java
+++ src/test/java/de/kosit/xmlmutate/runner/MutationRunnerTest.java
@@ -4,6 +4,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import de.kosit.xmlmutate.TestHelper;
+import de.kosit.xmlmutate.TestResource;
+import de.kosit.xmlmutate.mutation.Mutation;
+import de.kosit.xmlmutate.mutation.MutationDocumentContext;
 import java.net.URI;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -13,15 +17,11 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-
-import de.kosit.xmlmutate.TestResource;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.tuple.Pair;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import de.kosit.xmlmutate.TestHelper;
-import de.kosit.xmlmutate.mutation.Mutation;
+import org.w3c.dom.ProcessingInstruction;
 
 /**
  * @author Victor del Campo
@@ -40,10 +40,10 @@ public class MutationRunnerTest {
                 Paths.get(uri));
         assertThat(mutations).hasSize(2);
         assertThat(mutations.get(0).getState()).isEqualTo(Mutation.State.CREATED);
-        assertThat(mutations.get(0).getMutationErrorContainer().getGlobalErrorMessages().size()).isEqualTo(0);
+        assertThat(mutations.get(0).getMutationErrorContainer().getGlobalErrorMessages()).isEmpty();
         // #46 "Tough question. I would say it should NOT lead to an error mutation"
         assertThat(mutations.get(1).getState()).isEqualTo(Mutation.State.CREATED);
-        assertThat(mutations.get(1).getMutationErrorContainer().getGlobalErrorMessages().size()).isGreaterThanOrEqualTo(1);
+        assertThat(mutations.get(1).getMutationErrorContainer().getGlobalErrorMessages()).isNotEmpty();
         assertThat(mutations.get(1).getMutationErrorContainer().getGlobalErrorMessages().stream()
                 .anyMatch(e -> StringUtils.containsIgnoreCase(e.getMessage(), "Mutation instruction id was already declared"))).isTrue();
     }
@@ -130,7 +130,7 @@ public class MutationRunnerTest {
     }
 
     @Test
-    @DisplayName("Test with fail fast failure mode and an action error in the 2nd mutation of total of 2")
+    @DisplayName("Test with fail fast failure mode and an action error with the 2nd failed mutation of total of 2")
     public void testFailFastActionError2ndMutation() {
         final URI uri = TestResource.BookResources.ACTION_ERROR_2ND_MUTATION;
         final RunnerConfig runnerConfig = TestHelper.createRunnerConfig(uri, FailureMode.FAIL_FAST);
@@ -141,6 +141,65 @@ public class MutationRunnerTest {
             assertThat(resultFuture.get().getValue()).hasSize(2);
         } catch (final InterruptedException | ExecutionException e) {
             e.printStackTrace();
+        }
+    }
+
+    @Test
+    @DisplayName("Should fail fast right with the very first invalid mutator definition skipping all subsequent mutators")
+    public void testFailFastActionErrorWith3rdErroneousMutator() {
+        final URI uri = TestResource.BookResources.ACTION_ERROR_3RD_MUTATION;
+        final RunnerConfig runnerConfig = TestHelper.createRunnerConfig(uri, FailureMode.FAIL_FAST);
+        final MutationRunner runner = new MutationRunner(runnerConfig, this.executor);
+
+        final Future<Pair<Path, List<Mutation>>> resultFuture = runner.process(Paths.get(uri));
+        try {
+            List<Mutation> result = resultFuture.get().getValue();
+            assertThat(result.stream()
+                .map(Mutation::getContext)
+                .map(MutationDocumentContext::getPi)
+                .map(ProcessingInstruction::getData))
+                .isNotEmpty()
+                .hasSize(3)
+                .containsExactly(
+                    "mutator=\"remove\" schema-valid schematron-invalid=\"schematron:Book-1\" id=\"id1\"",
+                    "mutator=\"identity\" schema-valid id=\"id11\"",
+                    "mutator=\"remove\" shema-valid shematron-valid=\"schematron:Book-2\" id=\"id1\""
+                );
+        } catch (final InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+            if (Thread.interrupted()) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("Should parse all mutators including erroneous and running all only valid mutators")
+    public void testFailAtEndActionErrorWith3rdErroneousMutator() {
+        final URI uri = TestResource.BookResources.ACTION_ERROR_3RD_MUTATION;
+        final RunnerConfig runnerConfig = TestHelper.createRunnerConfig(uri, FailureMode.FAIL_AT_END);
+        final MutationRunner runner = new MutationRunner(runnerConfig, this.executor);
+
+        final Future<Pair<Path, List<Mutation>>> resultFuture = runner.process(Paths.get(uri));
+        try {
+            List<Mutation> result = resultFuture.get().getValue();
+            assertThat(result.stream()
+                .map(Mutation::getContext)
+                .map(MutationDocumentContext::getPi)
+                .map(ProcessingInstruction::getData))
+                .isNotEmpty()
+                .hasSize(4)
+                .containsExactly(
+                    "mutator=\"remove\" schema-valid schematron-invalid=\"schematron:Book-1\" id=\"id1\"",
+                    "mutator=\"identity\" schema-valid id=\"id11\"",
+                    "mutator=\"remove\" shema-valid shematron-valid=\"schematron:Book-2\" id=\"id1\"",
+                    "mutator=\"identity\" schema-valid id=\"id12\""
+                );
+        } catch (final InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+            if (Thread.interrupted()) {
+                Thread.currentThread().interrupt();
+            }
         }
     }
 
